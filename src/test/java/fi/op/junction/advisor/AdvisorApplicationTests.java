84fb46d1c3ea8df83d2026c160f15d0464fb27f1
kattiehudec <kattiehudec@gmail.com>
2020-11-06 20:00:22 +0300

    init

---
+++ src/test/java/fi/op/junction/advisor/AdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.op.junction.advisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvisorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
